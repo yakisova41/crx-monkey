97ee91fa342f34c9189cf68ce9cb005d5fabd1db
yakisova41 <yakisoba5emon@gmail.com>
2024-03-31 01:37:25 +0900

    🐛 fix unexpect reload when update contentscript

--- packages/crx-monkey/src/node/handlers/dev/create-client/create-dev-extension.ts
+++ packages/crx-monkey/src/node/handlers/dev/create-client/create-dev-extension.ts
@@ -2,17 +2,27 @@ import fse from 'fs-extra';
 import path from 'path';
 import { getConfig } from 'src/node/config';
 import { ManifestFactory } from 'src/node/manifest-factory';
+import { createMatchMap, getAllJsAndCSSByContentScripts } from 'src/node/manifest-factory/utils';
 
 export function createDevExtension(factory: ManifestFactory) {
   const fileName = 'crx-monkey-contentscript.js';
 
   const config = getConfig();
+  const manifestWorkspace = factory.getWorkspace();
+  const contentScripts = manifestWorkspace.content_scripts;
 
-  const contentScriptPath = path.join(config.chromeOutputDir, fileName);
+  if (contentScripts !== undefined) {
+    const contentScriptPath = path.join(config.chromeOutputDir, fileName);
 
-  fse.outputFile(contentScriptPath, generateContentScript(config.devServer));
+    fse.outputFile(contentScriptPath, generateContentScript(config.devServer));
 
-  factory.addContentScript([fileName], [], ['https://*/*', 'http://*/*']);
+    if (contentScripts !== undefined) {
+      const { jsFiles, cssFiles } = getAllJsAndCSSByContentScripts(contentScripts);
+      const { allMatches } = createMatchMap(contentScripts, jsFiles, cssFiles);
+
+      factory.addContentScript([fileName], [], [...allMatches]);
+    }
+  }
 }
 
 function generateContentScript(devServer: { port: number; host: string; websocket: number }) {
