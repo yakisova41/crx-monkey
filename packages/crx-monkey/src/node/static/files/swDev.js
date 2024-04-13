1ff15a054e4363be263c879be0ce8c98befd947c
yakisova41 <yakisoba5emon@gmail.com>
2024-04-14 04:04:35 +0900

    🐛 Fix: Auto reload does not work

--- packages/crx-monkey/src/node/static/files/swDev.js
+++ packages/crx-monkey/src/node/static/files/swDev.js
@@ -1,18 +1,28 @@
 /* eslint-disable no-undef */
 
-const websocket = new WebSocket(`ws://${host}:${websocket}`);
+function startWebsocket25secLife() {
+  const websocket = new WebSocket(`ws://${host}:${websocket}`);
 
-websocket.addEventListener('message', ({ data }) => {
-  switch (data) {
-    case 'RELOAD_CONTENT_SCRIPT':
-    case 'RELOAD_SW':
-    case 'RELOAD_CSS':
-    case 'RELOAD_POPUP_JS':
-    case 'RELOAD_POPUP_HTML':
-      chrome.runtime.reload();
-      break;
+  websocket.addEventListener('message', ({ data }) => {
+    switch (data) {
+      case 'RELOAD_CONTENT_SCRIPT':
+      case 'RELOAD_SW':
+      case 'RELOAD_CSS':
+      case 'RELOAD_POPUP_JS':
+      case 'RELOAD_POPUP_HTML':
+        chrome.runtime.reload();
+        break;
 
-    default:
-      break;
-  }
-});
+      default:
+        break;
+    }
+  });
+  setTimeout(() => {
+    websocket.close();
+  }, 25 * 1000);
+}
+
+startWebsocket25secLife();
+setInterval(() => {
+  startWebsocket25secLife();
+}, 25 * 1000);
