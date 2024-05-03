c785a4b8ab38591a4e0f619492debc863a628bcd
yakisova41 <yakisoba5emon@gmail.com>
2024-05-04 01:31:16 +0900

    🔨 update dev script

--- dev/src/contentScript/contentScript.ts
+++ dev/src/contentScript/contentScript.ts
@@ -1,5 +1,32 @@
 import { namedModule } from './module';
-import { getPublicUrl, getRuntime } from '../../../packages/crx-monkey/dist/client/main';
-console.log(getRuntime());
-console.log(getPublicUrl());
+import {
+  bypassMessage,
+  bypassSendMessage,
+  getExtensionId,
+  getRunningRuntime,
+} from '../../../packages/crx-monkey/dist/client/main';
+
+console.log('!!Content script 1 has loaded.');
+
+const runtime = getRunningRuntime();
+
+console.log('Running by:', runtime);
+
+if (runtime === 'Extension') {
+  /**
+   * Script running in MAIN world, if you use bypass function, you can bypass the feature of runtime.
+   */
+  getExtensionId().then((id) => {
+    console.log('Extension id:', id);
+  });
+
+  bypassMessage((msg) => {
+    console.log('Receved a message by service worker.');
+    console.log(msg);
+  });
+
+  console.log('Send a message to service worker.');
+  bypassSendMessage({ msg: 'Hi' });
+}
+
 namedModule();
--- dev/src/sw/sw.ts
+++ dev/src/sw/sw.ts
@@ -1,1 +1,12 @@
 console.log('service worker');
+
+chrome.runtime.onMessage.addListener((e, sender) => {
+  console.log('Receved a message by content script', e);
+
+  const id = sender.tab?.id;
+
+  if (id !== undefined) {
+    console.log(sender);
+    chrome.runtime.sendMessage(sender.id, { msg: 'Hi!! from sw' }, {}, () => {});
+  }
+});
