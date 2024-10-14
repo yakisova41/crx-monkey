98f873ff479cf563938a933514f7b21762465d92
yakisova41 <yakisoba5emon@gmail.com>
2024-10-14 23:14:37 +0900

    🎨 ConsoleApp class common modularization.

--- packages/crx-monkey/src/node/main.ts
+++ packages/crx-monkey/src/node/main.ts
@@ -2,10 +2,11 @@ import { loadConfig } from './config';
 import { ConsoleApp } from './console';
 import build from './handlers/build';
 import dev from './handlers/dev';
+import pkg from '../../package.json';
 
 await loadConfig();
 
-const app = new ConsoleApp();
+const app = new ConsoleApp('CRX MONKEY', pkg.version);
 app.addCommand('build', build, []);
 app.addCommand('dev', dev, []);
 app.run();
